ff5a5321f7550ade41c6363f80057ae8a04adb33
OXIDE <oxidiu777@gmail.com>
2023-03-05 21:11:55 +0200

    Debugged

--- gpt3_jsm/src/components/navbar/Navbar.jsx
+++ gpt3_jsm/src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {RiMenu3Line, RiCloseLine} from "react-icons/ri"; //asta nu stiu sigur pentru ce le importam
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.svg";
 import "./navbar.css"
 
 const Navbar = () => {
@@ -8,7 +8,14 @@ const Navbar = () => {
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src="logo" alt="logo"/>
+          <img src={logo} alt="logo"/>
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT-3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
       
